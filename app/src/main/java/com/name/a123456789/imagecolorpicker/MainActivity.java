8ed05434c795671f8b590a7c3ba7d26e1daadf80
syorito-hatsuki <enderdron3@gmail.com>
2020-02-26 16:36:35 +0200

    Moved to github repo

---
+++ app/src/main/java/com/name/a123456789/imagecolorpicker/MainActivity.java
@@ -0,0 +1,108 @@
+package com.name.a123456789.imagecolorpicker;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.graphics.ColorUtils;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.graphics.ColorFilter;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.text.Html;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Objects;
+import java.util.jar.Manifest;
+
+import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView imageView;
+    private static final int GALLERY_REQUEST = 100;
+
+    @SuppressLint("ClickableViewAccessibility")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.imageView);
+
+        FloatingActionButton floatingActionButton = findViewById(R.id.floatingActionButton);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+                photoPickerIntent.setType("image/*");
+                startActivityForResult(photoPickerIntent, 100);
+            }
+        });
+
+        imageView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                try {
+                    imageView.setDrawingCacheEnabled(true);
+                    final Bitmap ibm = Bitmap.createBitmap(imageView.getDrawingCache());
+                    imageView.setDrawingCacheEnabled(false);
+
+                    int pxl = ibm.getPixel((int) event.getX(), (int) event.getY());
+
+                    int redValue = Color.red(pxl);
+                    int greenValue = Color.green(pxl);
+                    int blueValue = Color.blue(pxl);
+                    setTitle(Html.fromHtml("<font color='#" + Integer.toHexString(pxl^0xffffff).substring(2) + "'>" + "RGB: " + redValue + " " + greenValue + " " + blueValue + " | #" + Integer.toHexString(pxl).substring(2) + "</font>"));
+                    Objects.requireNonNull(getSupportActionBar()).setBackgroundDrawable(new ColorDrawable(Color.parseColor("#" + Integer.toHexString(pxl).substring(2))));
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                        Window window = getWindow();
+                        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+                        window.setStatusBarColor(pxl);
+                    }
+                    ibm.recycle();
+                } catch (Exception ignore) { }
+                return true;
+            }
+        });
+    }
+
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == Activity.RESULT_OK)
+            if (requestCode == GALLERY_REQUEST) {
+                Uri selectedImage = data.getData();
+                try {
+                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), selectedImage);
+                    imageView.setImageBitmap(bitmap);
+                } catch (IOException e) {
+                    Log.i("TAG", "Some exception " + e);
+                }
+            }
+    }
+}
