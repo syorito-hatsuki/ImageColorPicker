3d669fa2daf52524593b62e0d0edc1c353e8785b
syorito-hatsuki <enderdron3@gmail.com>
2020-02-26 18:15:07 +0200

    Hex value moved to sub title

--- app/src/main/java/com/name/a123456789/imagecolorpicker/MainActivity.java
+++ app/src/main/java/com/name/a123456789/imagecolorpicker/MainActivity.java
@@ -1,6 +1,7 @@
 package com.name.a123456789.imagecolorpicker;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.app.ActivityCompat;
@@ -42,6 +43,7 @@ import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
 
 public class MainActivity extends AppCompatActivity {
     ImageView imageView;
+    ActionBar actionBar;
     private static final int GALLERY_REQUEST = 100;
 
     @SuppressLint("ClickableViewAccessibility")
@@ -49,6 +51,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (getSupportActionBar() != null){
+            actionBar = getSupportActionBar();
+        }
 
         imageView = findViewById(R.id.imageView);
 
@@ -72,11 +78,15 @@ public class MainActivity extends AppCompatActivity {
 
                     int pxl = ibm.getPixel((int) event.getX(), (int) event.getY());
 
+                    String hex = "#" + Integer.toHexString(pxl).substring(2);
+                    String invertedHex = "#" + Integer.toHexString(pxl^0xffffff).substring(2);
+
                     int redValue = Color.red(pxl);
                     int greenValue = Color.green(pxl);
                     int blueValue = Color.blue(pxl);
-                    setTitle(Html.fromHtml("<font color='#" + Integer.toHexString(pxl^0xffffff).substring(2) + "'>" + "RGB: " + redValue + " " + greenValue + " " + blueValue + " | #" + Integer.toHexString(pxl).substring(2) + "</font>"));
-                    Objects.requireNonNull(getSupportActionBar()).setBackgroundDrawable(new ColorDrawable(Color.parseColor("#" + Integer.toHexString(pxl).substring(2))));
+                    setTitle(Html.fromHtml("<font color='" + invertedHex + "'>" + "RGB: " + redValue + " " + greenValue + " " + blueValue + "</font>"));
+                    actionBar.setSubtitle(Html.fromHtml("<font color='" + invertedHex + "'>" + hex + "</font>"));
+                    actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor(hex)));
                     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
                         Window window = getWindow();
                         window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
